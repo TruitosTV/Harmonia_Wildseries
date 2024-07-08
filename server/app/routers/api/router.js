86364305d3d0c9312d47d4e5c6d30a3a213335ce
TruitosTV <axel.remontpro@gmail.com>
2024-07-08 17:17:49 +0200

    add the router route

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+
+const { wildSeries } = require("../../controllers/sayActions");
+
+router.get("/", wildSeries);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
@@ -9,6 +14,10 @@ const router = express.Router();
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
